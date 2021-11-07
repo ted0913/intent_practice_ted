ad8c6956d50e5e8b62f408fec60dcde58126a462
TED <newlife913@naver.com>
2021-11-08 00:20:10 +0900

    입력 문구 첨부

--- app/src/main/java/com/kgschem/intent_practice_ted/MainActivity.kt
+++ app/src/main/java/com/kgschem/intent_practice_ted/MainActivity.kt
@@ -10,6 +10,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        sendmessageBtn.setOnClickListener {
+
+            //입력한 내용을 변수에 저장.            
+            val inputmessage = messageEdt.text.toString()
+            
+            //비행기 티켓 발권            
+            val myintent = Intent(this, messageActivity::class.java)
+            
+            //수하물 첨부            
+            myintent.putExtra("message", inputmessage)
+            
+            //실제출발
+            startActivity(myintent)
+
+            
+        }
+
         moveToOtherBtn.setOnClickListener {
 
             //다른화면으로 이동(OtherActivity)
