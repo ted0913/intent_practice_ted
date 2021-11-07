07580e4ce79a62f2f53c587285371a184455f55b
TED <newlife913@naver.com>
2021-11-08 00:14:30 +0900

    메세지 보내느 ㄴ화면/ 받는 화면

--- app/src/main/java/com/kgschem/intent_practice_ted/MainActivity.kt
+++ app/src/main/java/com/kgschem/intent_practice_ted/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.kgschem.intent_practice_ted
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToOtherBtn.setOnClickListener {
+
+            //다른화면으로 이동(OtherActivity)
+
+            val MyIntent = Intent(this, OtherActivity::class.java)
+            startActivity(MyIntent)
+
+        }
     }
 }
--- app/src/main/java/com/kgschem/intent_practice_ted/OtherActivity.kt
+++ app/src/main/java/com/kgschem/intent_practice_ted/OtherActivity.kt
@@ -1,11 +1,24 @@
 package com.kgschem.intent_practice_ted
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+
+        returntomainbtn.setOnClickListener {
+
+
+//            val myintent = Intent(this, MainActivity::class.java)
+////            startActivity(myintent)
+
+            finish()
+
+        }
     }
 }
