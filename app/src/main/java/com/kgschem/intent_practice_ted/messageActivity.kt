0443aa912f5f34eb3b990b2e856b3ff79011aacb
TED <newlife913@naver.com>
2021-11-08 00:25:51 +0900

    전달해준 문구 전달

--- app/src/main/java/com/kgschem/intent_practice_ted/messageActivity.kt
+++ app/src/main/java/com/kgschem/intent_practice_ted/messageActivity.kt
@@ -7,5 +7,13 @@ class messageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
+
+        //이 화면에 들어올때 첨부된 데이터를 텍스트뷰에 반영
+
+        val intentmessage = intent.getStringExtra("message")
+
+        recei
+        receivedMessageTxt.text = intentmessage
+
     }
 }
